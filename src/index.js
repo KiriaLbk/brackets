4ce6ea25e9fa9d066c41d99d6137788d94664332
Kirill Lobko <lobko.messi4@gmail.com>
2020-03-09 21:00:56 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    for (let index = 0; index < bracketsConfig.length+1; index++) {
+        for (let i = 0; i < bracketsConfig.length; i++) {
+            while (str.split('').findIndex((item, index, array)=>array[index]==bracketsConfig[i][0] && array[index+1]==bracketsConfig[i][1])!=-1) {
+                str=str.replace(`${bracketsConfig[i][0]}${bracketsConfig[i][1]}`,"");
+            }        
+            if(str=='')
+            {
+                return true;
+            }
+        }
+    }
+    return false;
 }
